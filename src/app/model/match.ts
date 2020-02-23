7538acf70469c05a704f74923d536ef1526c731e
mat <m.hemmerle@adhoc-gti.com>
2020-02-23 09:18:54 +0100

    Mise en place formule

--- src/app/model/match.ts
+++ src/app/model/match.ts
@@ -1,5 +1,6 @@
 import { Equipe } from './equipe';
 import { Rencontre } from './rencontre';
+import { FormuleService } from '../services/formule.service';
 
 export class Match {
     constructor (tailleEquipe: number) {
@@ -7,9 +8,13 @@ export class Match {
         this.equipeVisiteuse = new Equipe(tailleEquipe, 'U');
         this.rencontres = new Array();
         
-        for (let index = 0; index < 20; index++) {
-            this.rencontres.push(new Rencontre(this.equipeReceveuse.joueurs[0], this.equipeVisiteuse.joueurs[0]));
-        }
+        var formule = new FormuleService().getFormule();
+        
+        formule.forEach(element => {
+            var joueurEquipeReceveuse = this.equipeReceveuse.getJoueurByPoisition(element[0]);
+            var joueurEquipeVisiteuse = this.equipeVisiteuse.getJoueurByPoisition(element[0]);
+            this.rencontres.push(new Rencontre(joueurEquipeReceveuse, joueurEquipeVisiteuse));
+        });
     }
 
     equipeReceveuse: Equipe
