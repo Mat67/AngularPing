b017609dc73f1d475add759a780bff0a62d5effa
mat <m.hemmerle@adhoc-gti.com>
2020-02-23 08:55:03 +0100

    Ajout tests supplémentaires

--- src/app/model/joueur.spec.ts
+++ src/app/model/joueur.spec.ts
@@ -8,4 +8,16 @@ describe('Joueur', () => {
   it('joueur 2 equipe A à la lettre B', () => {
     expect(new Joueur(2, 'A').id).toBe('B');
   });
+
+  it('joueur 6 equipe A à la lettre E', () => {
+    expect(new Joueur(2, 'A').id).toBe('E');
+  });
+
+  it('joueur 3 equipe X à la lettre V', () => {
+    expect(new Joueur(3, 'U').id).toBe('W');
+  });
+
+  it('joueur 6 equipe X à la lettre V', () => {
+    expect(new Joueur(3, 'U').id).toBe('Z');
+  });
 });
