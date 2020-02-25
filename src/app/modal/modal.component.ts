1ce622f5abc4d13e4634151f6a18aed8359d8813
mat <m.hemmerle@adhoc-gti.com>
2020-02-25 19:32:02 +0100

    Gestion champ signature

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -17,7 +17,7 @@ export class ModalComponent implements OnInit {
   ngOnInit(): void {
   }
   open(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+    this.modalService.open(content, { size: 'xl', centered: true }).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
