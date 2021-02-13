e25e5019935fa9b41c544892667dc9c521bf6f31
Christophe Fischer <christophe.fischer@adesso.de>
2021-02-13 15:55:19 +0100

    Imprimer et Random dans bottom

--- src/app/bottom-bar/bottom-bar.component.ts
+++ src/app/bottom-bar/bottom-bar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Match } from '../model/match';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { RandomService } from '../services/random.service';
+import { RencontreDouble } from '../model/rencontre-double';
 
 @Component({
   selector: 'app-bottom-bar',
@@ -13,5 +15,41 @@ export class BottomBarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public imprimer() {
+    window.print();
+  }
+
+  public random() {
+    var randomService = new RandomService;
+
+    this.match.equipeReceveuse.nomEquipe = randomService.getRandomString() + ' ' + randomService.random(1,5);
+    this.match.equipeVisiteuse.nomEquipe = randomService.getRandomString() + ' ' + randomService.random(1,5);
+
+    this.match.equipeReceveuse.joueurs.forEach(joueur => {
+      joueur.nom = randomService.getRandomString();
+      joueur.numeroLicence = randomService.random(100000, 999999).toString();
+      joueur.classement = randomService.random(0, 20).toString();
+    });
+    this.match.equipeVisiteuse.joueurs.forEach(joueur => {
+      joueur.nom = randomService.getRandomString();
+      joueur.numeroLicence = randomService.random(100000, 999999).toString();
+      joueur.classement = randomService.random(0, 20).toString();
+    });
+
+    this.match.rencontres.forEach(rencontre => {
+      rencontre.manches.forEach(manche => manche.score = '');
+
+      rencontre.manches.forEach(manche => {
+        if (rencontre.getVainqueur() === '')
+          manche.score = randomService.getRandomScore();
+
+        if (rencontre instanceof RencontreDouble) {
+          (rencontre as RencontreDouble).doubleEquipeReceveuse = randomService.getRandomString();
+          (rencontre as RencontreDouble).doubleEquipeVisiteuse = randomService.getRandomString()
+        }
+      });
+    });
   }
 }
--- src/app/entete/entete.component.ts
+++ src/app/entete/entete.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Match } from '../model/match';
-import { RandomService } from '../services/random.service';
-import { RencontreDouble } from '../model/rencontre-double';
-import * as Mustache from 'mustache';
 
 @Component({
   selector: 'app-entete',
@@ -15,41 +12,5 @@ export class EnteteComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
-  
-  public imprimer() {
-    window.print();
-  }
-
-  public random() {
-    var randomService = new RandomService;
-
-    this.match.equipeReceveuse.nomEquipe = randomService.getRandomString() + ' ' + randomService.random(1,5);
-    this.match.equipeVisiteuse.nomEquipe = randomService.getRandomString() + ' ' + randomService.random(1,5);
-
-    this.match.equipeReceveuse.joueurs.forEach(joueur => {
-      joueur.nom = randomService.getRandomString();
-      joueur.numeroLicence = randomService.random(100000, 999999).toString();
-      joueur.classement = randomService.random(0, 20).toString();
-    });
-    this.match.equipeVisiteuse.joueurs.forEach(joueur => {
-      joueur.nom = randomService.getRandomString();
-      joueur.numeroLicence = randomService.random(100000, 999999).toString();
-      joueur.classement = randomService.random(0, 20).toString();
-    });
-
-    this.match.rencontres.forEach(rencontre => {
-      rencontre.manches.forEach(manche => manche.score = '');
-
-      rencontre.manches.forEach(manche => {
-        if (rencontre.getVainqueur() === '')
-          manche.score = randomService.getRandomScore();
-
-        if (rencontre instanceof RencontreDouble) {
-          (rencontre as RencontreDouble).doubleEquipeReceveuse = randomService.getRandomString();
-          (rencontre as RencontreDouble).doubleEquipeVisiteuse = randomService.getRandomString()
-        }
-      });
-    });
   }
 }
