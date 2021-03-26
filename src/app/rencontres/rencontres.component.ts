bce98d4a994e8af6bd77ec1724c2927485c9a397
Mathieu HEMMERLE <m.hemmerle@groupe-burrus.tech>
2021-03-26 20:24:32 +0100

    Controle de saisie

--- src/app/rencontres/rencontres.component.ts
+++ src/app/rencontres/rencontres.component.ts
@@ -18,4 +18,23 @@ export class RencontresComponent implements OnInit {
     this.match = new Match(6);
   }
 
+  omit_number(score, evt):boolean {
+    var charValid =  (evt.key === '-' || !isNaN(evt.key))
+    if (charValid) {
+      var separateur = score.indexOf('-')
+
+      
+      if (evt.key === '-' && separateur !== -1)
+        return false // Pas le droit d'avoir plus d'1 occurence de '-'
+      else if (evt.key === '-' && separateur === -1 && score.split('-')[0].length === 0) 
+        return false // impossible de saisir '-' si on a pas au moins 1 nombre sur la partie de gauche
+      else if (!isNaN(evt.key)) {
+        var partieGaucheValide = score.split('-')[0].length < 2 || separateur !== -1
+        var partieDroiteValide = separateur === -1 || score.split('-')[1].length < 2
+        return partieGaucheValide && partieDroiteValide
+      } 
+    }
+
+    return charValid
+  }
 }
