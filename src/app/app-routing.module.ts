716abcd806e81a697099eb418cb3b8667177df03
HEMMERLE Mathieu <M.HEMMERLE@groupe-burrus.tech>
2023-03-06 18:26:36 +0100

    build pour github page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { MatchDetailComponent } from './match-detail/match-detail.component';
 import { SignatureMobileComponent } from './signature-mobile/signature-mobile.component';
 
 const routes: Routes = [
-  { path: '', component: MatchDetailComponent },
-  { path: 'matchs/:matchId', component: MatchDetailComponent },
   { path: 'matchs/:matchId/equipes/:equipeId/signature', component: SignatureMobileComponent },
+  { path: 'matchs/:matchId', component: MatchDetailComponent },
+  { path: '', component: HomeComponent }
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
