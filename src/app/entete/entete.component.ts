8307cd6fac332bb933e04142aed16c861198f492
mat <hemmerle.mathieu@gmail.com>
2023-09-19 21:36:14 +0200

    Format date

--- src/app/entete/entete.component.ts
+++ src/app/entete/entete.component.ts
@@ -1,10 +1,60 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, Injectable } from '@angular/core';
 import { Match } from '../model/match';
+import { NgbDateAdapter, NgbDateParserFormatter, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+
+@Injectable()
+export class CustomAdapter extends NgbDateAdapter<string> {
+	readonly DELIMITER = '-';
+
+	fromModel(value: string | null): NgbDateStruct | null {
+		if (value) {
+			const date = value.split(this.DELIMITER);
+			return {
+				day: parseInt(date[0], 10),
+				month: parseInt(date[1], 10),
+				year: parseInt(date[2], 10),
+			};
+		}
+		return null;
+	}
+
+	toModel(date: NgbDateStruct | null): string | null {
+		return date ? date.day + this.DELIMITER + date.month + this.DELIMITER + date.year : null;
+	}
+}
+
+/**
+ * This Service handles how the date is rendered and parsed from keyboard i.e. in the bound input field.
+ */
+@Injectable()
+export class CustomDateParserFormatter extends NgbDateParserFormatter {
+	readonly DELIMITER = '/';
+
+	parse(value: string): NgbDateStruct | null {
+		if (value) {
+			const date = value.split(this.DELIMITER);
+			return {
+				day: parseInt(date[0], 10),
+				month: parseInt(date[1], 10),
+				year: parseInt(date[2], 10),
+			};
+		}
+		return null;
+	}
+
+	format(date: NgbDateStruct | null): string {
+		return date ? ("0" + date.day).slice(0,2) + this.DELIMITER + ("0" + date.month).slice(0,2) + this.DELIMITER + date.year : '';
+	}
+}
 
 @Component({
   selector: 'app-entete',
   templateUrl: './entete.component.html',
   styleUrls: ['./entete.component.css'],
+  providers: [
+		{ provide: NgbDateAdapter, useClass: CustomAdapter },
+		{ provide: NgbDateParserFormatter, useClass: CustomDateParserFormatter },
+	],
 })
 export class EnteteComponent implements OnInit {
   @Input() match: Match;
