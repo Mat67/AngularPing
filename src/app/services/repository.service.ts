78ec650c88a0a281af4355e21ad821b0bf9d8898
Hemmerle Mathieu <hemmerle.mathieu@gmail.com>
2026-08-03 17:19:36 +0200

    corrections

--- src/app/services/repository.service.ts
+++ src/app/services/repository.service.ts
@@ -4,8 +4,8 @@ import { Match } from '../model/match';
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 import { Joueur } from '../model/joueur';
 
-//const WS_URL = 'wss://serveur-mat.synology.me:8989';
-const WS_URL = 'ws://localhost:8999';
+const WS_URL = 'wss://serveur-mat.synology.me:8989';
+//const WS_URL = 'ws://localhost:8999';
 
 export interface Message {
   author: string;
